6b1effc9cbf8ba9c8519876332f9cd2c4620bdde
i44p <jsbbdjzjsjawk+@gmail.com>
2023-04-08 19:21:03 +0300

    add tstack implementation

--- include/tstack.h
+++ include/tstack.h
@@ -2,9 +2,21 @@
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 
-template<typename T, int size>
+template <typename T, int size>
 class TStack {
-  // добавьте код стека
+ private:
+  T arr[size];
+  int top;
+
+ public:
+  TStack() : top(-1){};
+
+  bool isEmpty() const { return top == -1; };
+  bool isFull() const { return top == (size - 1); };
+
+  T get() const { return isEmpty() ? throw std::string("empty") : arr[top]; };
+  T pop() { return isEmpty() ? throw std::string("empty") : arr[top--]; };
+  void push(T v) { isFull() ? throw std::string("full") : arr[++top] = v; };
 };
 
 #endif  // INCLUDE_TSTACK_H_
