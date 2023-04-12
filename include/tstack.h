e5411ee77360b6a8e8d25f5217dd16f911e7dcbb
i44p <jsbbdjzjsjawk+@gmail.com>
2023-04-12 14:30:11 +0300

    improve tstack

--- include/tstack.h
+++ include/tstack.h
@@ -2,21 +2,23 @@
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 
-template <typename T, int size>
+template <typename T, int capacity>
 class TStack {
  private:
-  T arr[size];
+  T arr[capacity];
   int top;
 
  public:
   TStack() : top(-1){};
 
   bool isEmpty() const { return top == -1; };
-  bool isFull() const { return top == (size - 1); };
+  bool isFull() const { return size() == capacity; };
 
   T get() const { return isEmpty() ? throw std::string("empty") : arr[top]; };
   T pop() { return isEmpty() ? throw std::string("empty") : arr[top--]; };
   void push(T v) { isFull() ? throw std::string("full") : arr[++top] = v; };
+
+  int size() const { return top + 1; };
 };
 
 #endif  // INCLUDE_TSTACK_H_
