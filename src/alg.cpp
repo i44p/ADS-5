67c11cf17333797e63e5db195830d8f9b6df387f
i44p <jsbbdjzjsjawk+@gmail.com>
2023-05-28 00:26:38 +0300

    implement eval

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,8 @@
 // Copyright 2021 NNTU-CS
-#include <string>
+#include <cctype>
 #include <map>
+#include <string>
+
 #include "tstack.h"
 
 std::string infx2pstfx(std::string inf) {
@@ -8,7 +10,39 @@ std::string infx2pstfx(std::string inf) {
   return std::string("");
 }
 
+int grabNum(int &index, std::string str) {
+  int ret = 0;
+  while (index < str.size() && isdigit(str[index])) {
+    ret *= 10;
+    ret += str[index] - '0';
+    index++;
+  }
+  return ret;
+}
+
 int eval(std::string pref) {
-  // добавьте код
-  return 0;
+  TStack<int, 100> stack;
+
+  for (int i = 0; i < pref.size(); i++) {
+    if (isdigit(pref[i])) {
+      stack.push(grabNum(i, pref));
+      continue;
+    }
+    switch (pref[i]) {
+      case '+':
+        stack.push(stack.pop() + stack.pop());
+        break;
+      case '-':
+        stack.push(stack.pop() - stack.pop());
+        break;
+      case '*':
+        stack.push(stack.pop() * stack.pop());
+        break;
+      case '/':
+        stack.push(stack.pop() / stack.pop());
+        break;
+    }
+  }
+
+  return stack.get();
 }
